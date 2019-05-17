682d967d2f10421a502a2a42b526bcd5ee2b9725
阿东 <783331585@qq.com>
2019-05-17 10:49:19 +0800

    Create packages.cs

---
+++ CX4.Domain.Test/packages.cs
@@ -0,0 +1,10 @@
+<?xml version="1.0" encoding="utf-8"?>
+<packages>
+  <package id="Autofac" version="4.9.2" targetFramework="net472" />
+  <package id="Autofac.Extras.DynamicProxy" version="4.5.0" targetFramework="net472" />
+  <package id="Castle.Core" version="4.3.1" targetFramework="net472" />
+  <package id="MediatR" version="7.0.0" targetFramework="net472" />
+  <package id="MSTest.TestAdapter" version="1.3.2" targetFramework="net472" />
+  <package id="MSTest.TestFramework" version="1.3.2" targetFramework="net472" />
+  <package id="NLog" version="4.6.3" targetFramework="net472" />
+</packages>
