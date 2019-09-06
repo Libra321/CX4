e6a4e7f463877db96b07cd512391bc3e13dd3e5f
阿东 <783331585@qq.com>
2019-09-06 20:28:34 +0800

    Create InterceptorBase.cs

---
+++ CX4.Infrastructure/AOP/InterceptorBase.cs
@@ -0,0 +1,37 @@
+/********************************************************************************
+ ****** 创 建 者    ：pengdong
+ ****** 创建日期    ：2019/5/16 17:44:41
+ ****** 功能描述    ：InterceptorBase
+ *******************************************************************************/
+
+using Castle.DynamicProxy;
+
+namespace CX4.Infrastructure.AOP
+{
+    /// <summary>
+    /// 拦截器基类
+    /// </summary>
+    public abstract class InterceptorBase : IInterceptor
+    {
+        /// <summary>
+        /// 拦截前操作
+        /// </summary>
+        protected abstract void InterceptBefore(IInvocation invocation);
+
+        /// <summary>
+        /// 拦截后操作
+        /// </summary>
+        protected abstract void InterceptAfter(IInvocation invocation);
+
+        /// <summary>
+        ///拦截方法
+        /// </summary>
+        /// <param name="invocation"></param>
+        public void Intercept(IInvocation invocation)
+        {
+            InterceptBefore(invocation);
+            invocation.Proceed();
+            InterceptAfter(invocation);
+        }
+    }
+}
